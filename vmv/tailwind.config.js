266e1c5b64013088bc6e87a021adeb9c7d2b12fa
Karthikeyan <karthikeyan02092003@gmail.com>
2024-10-06 11:28:46 +0530

    Status : Started

---
+++ vmv/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Watch for JSX, TSX files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
