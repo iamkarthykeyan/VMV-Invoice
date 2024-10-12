bef454bfc493f560cb1de3d408cb54a414e7fb16
Karthikeyan <karthikeyan02092003@gmail.com>
2024-10-12 21:54:27 +0530

    Done and Dusted - Invoice Generated

--- vmv/src/Components/Dashboard/DashboardElements/HomeElement.jsx
+++ vmv/src/Components/Dashboard/DashboardElements/HomeElement.jsx
@@ -2,35 +2,36 @@ import React from 'react';
 
 const CardComponent = () => {
   return (
-<div className="flex flex-col bg-black rounded-3xl w-72 sm:w-80 md:w-96">
-      <div className="px-6 py-8 sm:p-10 sm:pb-6">
-        <div className="grid items-center justify-center w-full grid-cols-1 text-left">
-          <div>
-            <h2 className="text-lg font-medium tracking-tighter text-white lg:text-3xl">
-              Create Invoice
-            </h2>
-            <p className="mt-2 text-sm text-gray-100">Grow steadily and pizza.</p>
-          </div>
-          <div className="mt-6">
-            <p>
-              <span className="text-5xl font-light tracking-tight text-white">
-                $35
-              </span>
-              <span className="text-base font-medium text-white"> /mo </span>
-            </p>
-          </div>
-        </div>
+    // Create Invoice :
+<div className="flex flex-col bg-black rounded-3xl w-64 sm:w-72 md:w-80 lg:w-96">
+  <div className="px-4 py-6 sm:px-6 sm:py-8 md:p-10">
+    <div className="grid items-start justify-start w-full grid-cols-1 text-left">
+      <div>
+        <h2 className="font-medium tracking-tighter text-white text-5xl lg:text-4xl">
+          Create Invoice 📄
+        </h2>
+        <p className="mt-4 sm:mt-8 text-sm text-gray-100">Download as :</p>
       </div>
-      <div className="flex px-6 pb-8 sm:px-8">
-        <a
-          aria-describedby="tier-starter"
-          className="items-center justify-center w-full px-6 py-2.5 text-center text-black bg-white border-2 border-white rounded-full inline-flex duration-200 hover:bg-transparent hover:border-white hover:text-white focus:outline-none focus-visible:outline-white text-sm focus-visible:ring-white"
-          href="/form"
-        >
-          Get Started
-        </a>
+      <div className="mt-2">
+        <p>
+          <span className="text-2xl sm:text-3xl font-light tracking-tight text-white">
+            PDF, EXCEL & CSV
+          </span>
+        </p>
       </div>
     </div>
+  </div>
+  <div className="flex px-4 sm:px-6 pb-6 sm:pb-8">
+    <a
+      aria-describedby="tier-starter"
+      className="w-full px-4 py-2 text-center text-black bg-white border-2 border-white rounded-full inline-flex justify-center items-center duration-200 hover:bg-transparent hover:text-white focus:outline-none focus-visible:ring-white text-sm"
+      href="/form"
+    >
+      Get Started
+    </a>
+  </div>
+</div>
+
   );
 };
 
