dfa6a613e2e6ad9b72f7e83ce16021043291cba3
sribalaji0807 <sribalaji20040708@gmail.com>
2024-11-10 19:37:54 +0530

    deployment path error

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // Mounting routes
 app.use('/api/history', historyRoutes); // This line should be exactly as written
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../vmv/build', 'index.html'));
   });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
