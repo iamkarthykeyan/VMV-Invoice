2e4b780e6a7c258f22d1ba4d64a34f1220d2bf49
Karthikeyan <j.karthikeyandev@gmail.com>
2024-11-10 15:26:18 +0530

    Completed

--- vmv/src/Pages/Download/DownloadPdf.jsx
+++ vmv/src/Pages/Download/DownloadPdf.jsx
@@ -40,7 +40,7 @@ const DownloadPdf = ({ rows, formData, themeColor }) => {
 
     const saveBuyerInfo = async () => {
         try {
-            const response = await fetch('http://localhost:5000/api/saveInfo', {
+            const response = await fetch('http://localhost:5000/api/history/saveInfo', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
