79a0ebe5d4e12863d5fa9c213c1d0c71ba15d89f
Sribalaji0807 <137472784+Sribalaji0807@users.noreply.github.com>
2024-11-10 22:14:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const dotenv = require('dotenv');
+const helmet=require("helmet")
+dotenv.config();
 const connectDB = require('./config/db');
 const historyRoutes = require('./routes/historyinfo'); // Adjust path if it differs
 
 const app = express();
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        imgSrc: ["'self'",'http://localhost:5000/favicon.ico', 'https://ik.imagekit.io', 'https://static.vecteezy.com'], // Allow images from ik.imagekit.io and vecteezy.com
+        fontSrc: ["'self'", 'https://fonts.gstatic.com'], // Allow fonts from Google Fonts
+        styleSrc: ["'self'", 'https://fonts.googleapis.com', 'https://cdn.jsdelivr.net', "'unsafe-inline'"], // Allow styles from Google Fonts and inline styles
+        scriptSrc: ["'self'", 'https://cdn.tailwindcss.com', "'unsafe-inline'"], // Allow Tailwind CSS CDN and inline scripts
+        mediaSrc: ["'self'", 'https://ik.imagekit.io'], // Allow media files from ik.imagekit.io
+      },
+    })
+);
 
 // Connect to MongoDB
-connectDB();
+//connectDB();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+console.log(__dirname);
 // Mounting routes
 app.use('/api/history', historyRoutes); // This line should be exactly as written
 app.use(express.static(path.join(__dirname, '../vmv', 'dist')));
+
+// Catch-all route for all other requests (React routing)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../vmv', 'dist', 'index.html'));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
