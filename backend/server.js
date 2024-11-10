4476fdccc2375311bb402d2211daa88e3f5d9586
sribalaji0807 <sribalaji20040708@gmail.com>
2024-11-10 19:13:26 +0530

    deploy server code changes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const historyRoutes = require('./routes/historyinfo'); // Adjust path if it differs
@@ -14,6 +15,8 @@ app.use(express.json());
 
 // Mounting routes
 app.use('/api/history', historyRoutes); // This line should be exactly as written
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
